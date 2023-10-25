92674bd38bcfbebdb4889b4f9534522908c0f9b5
dipeshguptadeveloper <dipeshguptadeveloper@gmail.com>
2023-10-25 23:03:54 +0530

    coffee details activity added

---
+++ app/src/main/java/com/dkgtech/coffeehouse/CoffeeDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.dkgtech.coffeehouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee_detail)
+    }
+}
