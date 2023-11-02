7a088d94faa7ec7648253d5204d981512c9f50dc
dipeshguptadeveloper <dipeshguptadeveloper@gmail.com>
2023-11-02 08:35:16 +0530

    add to cart button functionality added

--- app/src/main/java/com/dkgtech/coffeehouse/CoffeeDetailActivity.kt
+++ app/src/main/java/com/dkgtech/coffeehouse/CoffeeDetailActivity.kt
@@ -3,6 +3,8 @@ package com.dkgtech.coffeehouse
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.dkgtech.coffeehouse.databinding.ActivityCoffeeDetailBinding
 import com.dkgtech.coffeehouse.databinding.FragmentCartBinding
 import com.dkgtech.coffeehouse.model.CoffeeCategoryModel
@@ -40,8 +42,23 @@ class CoffeeDetailActivity : AppCompatActivity() {
             }
 
             btnAddToCart.setOnClickListener {
-                startActivity(Intent(this@CoffeeDetailActivity, FragmentCartBinding::class.java))
+                var cartMap = mapOf<String, Any>(
+                    "catId" to coffeeCategoryModel.catId,
+                    "catImage" to coffeeCategoryModel.catImage,
+                    "catTitle" to coffeeCategoryModel.catTitle,
+                    "catSubTitle" to coffeeCategoryModel.catSubTitle,
+                    "catPrice" to coffeeCategoryModel.catPrice
+                )
+
+                db
+                    .collection("users")
+                    .document("5LjCmU5tQJhrtotgKypw")
+                    .collection("myCart")
+                    .add(cartMap).addOnSuccessListener {
+                        successful.visibility = View.VISIBLE
+                    }
             }
+
         }
     }
 }
